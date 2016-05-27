042c4ff26bd8541a2e49c34e7897315598eb15bd
chenxiao <529848371@qq.com>
2016-05-27 15:19:16 +0800

    Initial Commit

---
+++ GCD timer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD timer
+//
+//  Created by cyp on 16/5/27.
+//  Copyright © 2016年 XY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCD timer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GCD timer
+//
+//  Created by cyp on 16/5/27.
+//  Copyright © 2016年 XY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
